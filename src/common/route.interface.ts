b016ec033738239bbcdf9decc99e6e3421bab778
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-05-19 20:01:53 +0300

    feat: create route interface

---
+++ src/common/route.interface.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { ApiMethod } from './api.types';
+
+export interface IControllerRoute {
+  path: string;
+  method: ApiMethod;
+  function: (req: Request, res: Response, next: NextFunction) => void;
+}
