6b3967d46bd939513ff352f5dce01cd33f1cc52e
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-05-19 20:06:53 +0300

    feat: implement the main function with simple DI

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { App } from './app';
+import { ExceptionFilter } from './errors/exception.filter';
+import { LoggerService } from './logger/logger.service';
+import { UsersController } from './users/users.controller';
+
+const bootstrap = async () => {
+  const logger = new LoggerService();
+  const app = new App(
+    logger,
+    new UsersController(logger),
+    new ExceptionFilter(logger)
+  );
+  await app.init();
+};
+
+bootstrap();
